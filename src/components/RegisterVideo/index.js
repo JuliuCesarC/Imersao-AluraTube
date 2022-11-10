9c4c05291cb57ac093dbce6ea029c32cd761ca7e
Juliu Cesar Cardoso <juliu2012@gmail.com>
2022-11-10 19:20:19 -0300

    new

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { StyledRegisterVideo } from "./styles";
 
+function useForm(props) {
+	const [values, setValues] = React.useState(props.initialValue);
+	return {
+		values,
+		handleChange: (e) => {
+			const value = e.target.value;
+			const nomeDoCampo = e.target.name;
+			setValues({
+				...values,
+				[nomeDoCampo]: value,
+			});
+			// Utilizando o array com uma variável dentro de um objeto, podemos utilizar o valor dessa variável como nome do campo no objeto.
+		},
+		clearForm() {
+			setValues(props.initialValue);
+		},
+	};
+}
+
 function RegisterVideo() {
-	const [showForm, setShowForm] = React.useState(false);
+	const formState = useForm({
+		initialValue: { titulo: "", url: "" },
+	});
+	const [showForm, setShowForm] = React.useState(true);
 
 	return (
 		<StyledRegisterVideo>
@@ -15,9 +37,16 @@ function RegisterVideo() {
 				+
 			</button>
 			{showForm ? (
-				<form>
+				<form
+					onSubmit={(submit) => {
+						submit.preventDefault();
+						// setShowForm(false)
+						formState.clearForm();
+					}}
+				>
 					<div>
 						<button
+						type="button"
 							className="close-modal"
 							onClick={() => {
 								setShowForm(false);
@@ -25,8 +54,20 @@ function RegisterVideo() {
 						>
 							X
 						</button>
-						<input type="text" placeholder="Titulo do video" />
-						<input type="text" placeholder="URL" />
+						<input
+							type="text"
+							name="titulo"
+							placeholder="Titulo do video"
+							value={formState.values.titulo}
+							onChange={formState.handleChange}
+						/>
+						<input
+							type="text"
+							name="url"
+							placeholder="URL"
+							value={formState.values.url}
+							onChange={formState.handleChange}
+						/>
 						<button type="submit">Adicionar</button>
 					</div>
 				</form>
